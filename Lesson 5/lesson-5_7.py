d9ea3c897d33e6cbfcc527a114a82b62d38bc0c0
pkrivtsov <pkrivtsov@live.com>
2020-05-28 20:42:19 +0300

    corrected homework for lesson-5_7

--- Lesson 5/lesson-5_7.py
+++ Lesson 5/lesson-5_7.py
@@ -12,4 +12,4 @@ with open('example/text_7.txt', 'r', encoding='utf-8') as f_cool:
             content_b[a[0]] = int(a[2]) - int(a[3])
         content_c['average.profit'] = sum(sr_prib) / len(sr_prib)
         sr_prib = [content_b, content_c]
-        json.dump(sr_prib, g_cool)
+        json.dump(sr_prib, g_cool, ensure_ascii=False, indent=4)
